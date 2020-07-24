435ddd9c5a359ec819b310cdb576a3c715bf18ba
Xnuxersploit <62522733+Xnuvers007@users.noreply.github.com>
2020-07-25 06:36:29 +0700

    Fixed Typo

--- ddos/DDoS.py
+++ ddos/DDoS.py
@@ -19,7 +19,7 @@ bytes = random._urandom(1500)
 os.system("clear")
 os.system("figlet DDos Attack")
   
-result = pyfiglet.figlet_format("Xnuvers", font = "dotmatrix" ) 
+result = pyfiglet.figlet_format("XnuversXploitXen")
 print(result)
 print ('\n')
 
