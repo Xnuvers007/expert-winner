5834d4c64c1ea44f93c7a8f05ed93693064fa3b3
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-30 07:45:33 +0700

    CoronaTracker.php (Fixed typo)

--- Corona/Coronatracker.php
+++ Corona/Coronatracker.php
@@ -1,11 +1,21 @@
 <?php
 
+echo "__  __                              __  __      _       _ _  __  __          "
+echo "\ \/ /_ __  _   ___   _____ _ __ ___\ \/ /_ __ | | ___ (_) |_\ \/ /___ _ __  "
+echo " \  /| '_ \| | | \ \ / / _ \ '__/ __|\  /| '_ \| |/ _ \| | __|\  // _ \ '_ \ "
+echo " /  \| | | | |_| |\ V /  __/ |  \__ \/  \| |_) | | (_) | | |_ /  \  __/ | | |"
+echo "/_/\_\_| |_|\__,_| \_/ \___|_|  |___/_/\_\ .__/|_|\___/|_|\__/_/\_\___|_| |_|"
+echo "                                         |_|                                 "
+
+echo "Sekarang (now) = ".date('l, d / M / y');
+echo "Waktu  = ".date('H:i:s a');
+
 $url = file_get_contents('https://api.kawalcorona.com/indonesia/');
 $data = json_decode($url,true);
 
 echo "Negara : " . $data[0]['name'] . "\n";
 echo "Positif : " . $data[0]['positif'] . " Orang\n";
-echo "Sembuh : " . $data[0]['sembuh'] . " Orang\n>";
-echo "Meninggal : " . $data[0]['meninggal'] . " Orang\n>";
+echo "Sembuh : " . $data[0]['sembuh'] . " Orang\n";
+echo "Meninggal : " . $data[0]['meninggal'] . " Orang\n";
 
 ?>
