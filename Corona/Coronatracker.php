b151938f87dbf78a45fba2fd9d191f2deeae5959
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-30 07:28:32 +0700

    Update Coronatracker.php

--- Corona/Coronatracker.php
+++ Corona/Coronatracker.php
@@ -3,9 +3,9 @@
 $url = file_get_contents('https://api.kawalcorona.com/indonesia/');
 $data = json_decode($url,true);
 
-echo "Negara : " . $data[0]['name'] . "<br/>";
-echo "Positif : " . $data[0]['positif'] . " Orang <br/>";
-echo "Sembuh : " . $data[0]['sembuh'] . " Orang <br/>";
-echo "Meninggal : " . $data[0]['meninggal'] . " Orang <br/>";
+echo "Negara : " . $data[0]['name'] . "<br>";
+echo "Positif : " . $data[0]['positif'] . " Orang <br>";
+echo "Sembuh : " . $data[0]['sembuh'] . " Orang <br>";
+echo "Meninggal : " . $data[0]['meninggal'] . " Orang <br>";
 
 ?>
