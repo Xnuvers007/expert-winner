d8a70562bebe1ff8bc5ab4a64c6986890781171e
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-30 07:22:33 +0700

    Create Coronatracker.php

---
+++ Corona/Coronatracker.php
@@ -0,0 +1,11 @@
+<?php
+
+$url = file_get_contents('https://api.kawalcorona.com/indonesia/');
+$data = json_decode($url,true);
+
+echo "Negara : " . $data[0]['name'] . "<br/>";
+echo "Positif : " . $data[0]['positif'] . " Orang <br/>";
+echo "Sembuh : " . $data[0]['sembuh'] . " Orang <br/>";
+echo "Meninggal : " . $data[0]['meninggal'] . " Orang <br/>";
+
+?>
