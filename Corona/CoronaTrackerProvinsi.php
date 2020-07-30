f37f0cd5ef3a854bbc09a8b831a6f6acc4c30b7f
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-31 03:33:40 +0700

    Update CoronaTrackerProvinsi.php

--- Corona/CoronaTrackerProvinsi.php
+++ Corona/CoronaTrackerProvinsi.php
@@ -15,7 +15,7 @@ echo "FID : ". $_POST['FID'] . $data[0]['attributes']['FID'] . "\n";
 echo "Kode Provinsi : " . $_POST['Kode Provinsi'] . $data[0]['attributes']['Kode_Provi'] . "\n";
 echo "Provinsi : " . $_POST['Provinsi'] . $data[0]['attributes']['Provinsi'] . "\n";
 echo "Positif : " . $_POST['Positif'] . $data[0]['attributes']['Kasus_Posi'] . " Orang\n";
-echo "Sembuh : " . $_POST['Sembuh'] . $data[0]['attributes']['Kasus_Semb'] . "orang\n";
-echo "Meninggal : " . $_POST['Meninggal'] . $data[0]['attributes']['Kasus_Meni'] . "orang\n";
+echo "Sembuh : " . $_POST['Sembuh'] . $data[0]['attributes']['Kasus_Semb'] . " Orang\n";
+echo "Meninggal : " . $_POST['Meninggal'] . $data[0]['attributes']['Kasus_Meni'] . " Orang\n";
 
 ?>
