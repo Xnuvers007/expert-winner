40d84b9d295e5c11eee0ed4073e80c6758b197e4
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-30 09:35:17 +0700

    Update CoronaWorld.php

--- Corona/CoronaWorld.php
+++ Corona/CoronaWorld.php
@@ -7,13 +7,14 @@ $data = json_decode($url,true);
 $data1 = json_decode($url1,true);
 $data2 = json_decode($url2,true);
 
-echo $data[0]['name'] . "\n";
+echo "Total Positif : "$data[0]['name'] . "\n";
 echo "Positif : ". $data[0]['value'] . "\n";
 
-echo $data1[0]['name'] . "\n";
+echo "Total Sembuh : "$data1[0]['name'] . "\n";
 echo "Sembuh : ". $data1[0]['value'] . "\n";
 
-echo $data2[0]['name'] . "\n";
+echo "Total Meninggal : "$data2[0]['name'] . "\n";
 echo "Meninggal : ". $data2[0]['value'] . "\n";
+
 
 ?>
