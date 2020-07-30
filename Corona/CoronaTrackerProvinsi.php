24e09e0083ab161d29bd1d135e083eedd4f874f0
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-31 04:03:48 +0700

    Update CoronaTrackerProvinsi.php

--- Corona/CoronaTrackerProvinsi.php
+++ Corona/CoronaTrackerProvinsi.php
@@ -11,11 +11,14 @@ foreach($data as $d)
 
 echo "\n";
 
-echo "FID : ". $_POST['FID'] . $data[0]['attributes']['FID'] . "\n";
-echo "Kode Provinsi : " . $_POST['Kode Provinsi'] . $data[0]['attributes']['Kode_Provi'] . "\n";
-echo "Provinsi : " . $_POST['Provinsi'] . $data[0]['attributes']['Provinsi'] . "\n";
-echo "Positif : " . $_POST['Positif'] . $data[0]['attributes']['Kasus_Posi'] . " Orang\n";
-echo "Sembuh : " . $_POST['Sembuh'] . $data[0]['attributes']['Kasus_Semb'] . " Orang\n";
-echo "Meninggal : " . $_POST['Meninggal'] . $data[0]['attributes']['Kasus_Meni'] . " Orang\n";
+echo "FID : " . $data[0]['attributes']['FID'] . "\n";
+
+echo "Kode Provinsi : " . $data[0]['attributes']['Kode_Provi'] .  "\n";
+
+echo "Provinsi : " . $data[0]['attributes']['Provinsi'] . "\n";
+
+echo "Positif : " . $data[0]['attributes']['Kasus_Posi'] . " Orang\n";
+echo "Sembuh : " . $data[0]['attributes']['Kasus_Semb'] . " Orang\n";
+echo "Meninggal : ". $data[0]['attributes']['Kasus_Meni'] . " Orang\n";
 
 ?>
