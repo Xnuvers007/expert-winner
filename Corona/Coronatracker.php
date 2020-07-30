9f7c9b00c613b9da36337ccab73fbbcdf8800503
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-31 02:44:48 +0700

    Update Coronatracker.php

--- Corona/Coronatracker.php
+++ Corona/Coronatracker.php
@@ -3,22 +3,22 @@
 
 <?php
 
-echo "__  __                              __  __      _       _ _  __  __          "
-echo "\ \/ /_ __  _   ___   _____ _ __ ___\ \/ /_ __ | | ___ (_) |_\ \/ /___ _ __  "
-echo " \  /| '_ \| | | \ \ / / _ \ '__/ __|\  /| '_ \| |/ _ \| | __|\  // _ \ '_ \ "
-echo " /  \| | | | |_| |\ V /  __/ |  \__ \/  \| |_) | | (_) | | |_ /  \  __/ | | |"
-echo "/_/\_\_| |_|\__,_| \_/ \___|_|  |___/_/\_\ .__/|_|\___/|_|\__/_/\_\___|_| |_|"
-echo "                                         |_|                                 "
+echo "__  __                              __  __      _       _ _  __  __          ";
+echo "\ \/ /_ __  _   ___   _____ _ __ ___\ \/ /_ __ | | ___ (_) |_\ \/ /___ _ __  ";
+echo " \  /| '_ \| | | \ \ / / _ \ '__/ __|\  /| '_ \| |/ _ \| | __|\  // _ \ '_ \ ";
+echo " /  \| | | | |_| |\ V /  __/ |  \__ \/  \| |_) | | (_) | | |_ /  \  __/ | | |";
+echo "/_/\_\_| |_|\__,_| \_/ \___|_|  |___/_/\_\ .__/|_|\___/|_|\__/_/\_\___|_| |_|";
+echo "                                         |_|                                 ";
 
-echo "|-------------------------------------|"
-echo "| Author   : XNUVERS007               |"
-echo "| You Tube : https://bit.ly/Xnuvers   |"
-echo "| github   : https://bit.ly/Xnuvrs1   |"
-echo "| Facebook : https://bit.ly/Fesbuck   |"
-echo "| Instagram: https://bit.ly/Xnvrs13   |"
-echo "| Site     : http://bit.ly/Mykingbee  |"
-echo "|-------------------------------------|"
-echo "\n"
+echo "|-------------------------------------|";
+echo "| Author   : XNUVERS007               |";
+echo "| You Tube : https://bit.ly/Xnuvers   |";
+echo "| github   : https://bit.ly/Xnuvrs1   |";
+echo "| Facebook : https://bit.ly/Fesbuck   |";
+echo "| Instagram: https://bit.ly/Xnvrs13   |";
+echo "| Site     : http://bit.ly/Mykingbee  |";
+echo "|-------------------------------------|";
+echo "\n";
   
 echo "Sekarang (now) = ".date('l, d / M / y');
 echo "Waktu  = ".date('H:i:s a');
