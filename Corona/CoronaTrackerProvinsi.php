e0e620fe2bb71f0d4fbef1c4e850f3e63ebe4551
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-07-31 03:20:15 +0700

    Update CoronaTrackerProvinsi.php

--- Corona/CoronaTrackerProvinsi.php
+++ Corona/CoronaTrackerProvinsi.php
@@ -5,11 +5,10 @@ $data = json_decode($url,true);
 
 foreach($data as $d)
 {
-  echo $d['attributes']['FID'];
+  echo $d['attributes']['FID']"\n";
 }
 
 echo "FID : " . $data[0]['attributes']['FID'] . "\n";
-
 echo "Kode Provinsi : " . $data[0]['attributes']['Kode_Provi'] . "\n";
 echo "Provinsi : " . $data[0]['attributes']['Provinsi'] . "\n";
 echo "Positif : " . $data[0]['attributes']['Kasus_Posi'] . " Orang\n";
